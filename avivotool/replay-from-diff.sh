a3f9afe919d83a771a80eefe2989b1f59d8f5ba4
Daniel Stone <daniel@fooishbar.org>
2007-03-05 22:26:49 +0200

    avivotool: fix path in script

--- avivotool/replay-from-diff.sh
+++ avivotool/replay-from-diff.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-egrep '^\+' $1 | sed -e 's/^\+\([^ 	]*\)	\([^ 	]*\).*$/sudo .\/radeontool regset 0x\1 0x\2/;'
+egrep '^\+' $1 | sed -e 's/^\+\([^ 	]*\)	\([^ 	]*\).*$/sudo .\/avivotool regset 0x\1 0x\2/;'
