fc17fa764c7aef0232c21f5aa6ae2353d5efe7b7
Jerome Glisse <glisse@freedesktop.org>
2007-06-24 12:25:28 +0200

    avivo: fix a typo now truly assign LFP to TMDS2.

--- xorg/avivo_output.c
+++ xorg/avivo_output.c
@@ -320,7 +320,7 @@ avivo_output_init(ScrnInfoPtr screen_info, xf86ConnectorType type,
             break;
         }
     }
-    if (avivo_output->output_offset == XF86ConnectorLFP) {
+    if (avivo_output->type == XF86ConnectorLFP) {
         avivo_output->output_offset = AVIVO_TMDS2_CNTL - AVIVO_TMDS1_CNTL;
     }
 
